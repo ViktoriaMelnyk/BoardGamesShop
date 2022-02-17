4466c4c8e308601367c7acd46a06d40c2c9289c6
ViktoriaMelnyk <72021306+ViktoriaMelnyk@users.noreply.github.com>
2022-02-17 14:31:11 +0100

    Created Category Model, EF, Added Category to database

---
+++ BoardGamesShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BoardGamesShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoardGamesShop.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
