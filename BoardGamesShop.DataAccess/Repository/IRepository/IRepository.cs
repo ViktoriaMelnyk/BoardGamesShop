e23c792ac7143487de267cce7ed3c12c0d07f376
ViktoriaMelnyk <72021306+ViktoriaMelnyk@users.noreply.github.com>
2022-02-18 18:41:39 +0100

    IRepository, Repository

---
+++ BoardGamesShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGames.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T- Category
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll();
+        void Add(T item);
+        void Remove(T item);
+        void RemoveRange(IEnumerable<T> items);
+
+    }
+}
